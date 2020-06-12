2a794094917bc078a676207ff073634b90a655a2
quirijnslings <quirijn.slings@trivident.com>
2020-06-12 16:19:50 +0200

    Added update-environment, delete-environment and verbose option

--- src/Dyndle.Tools.Environments/Configuration/IEnvironmentsConfiguration.cs
+++ src/Dyndle.Tools.Environments/Configuration/IEnvironmentsConfiguration.cs
@@ -5,11 +5,6 @@ namespace Dyndle.Tools.Environments
     public interface IEnvironmentsConfiguration : ICoreConfiguration
     {
         string Name { get; set; }
-        string CMSUrl { get; set; }
-        string Username { get; set; }
-        string UserDomain { get; set; }
-        string Password { get; set; }
-        bool MakeDefault { get; set; }
 
     }
 }
